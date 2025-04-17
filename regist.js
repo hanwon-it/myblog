3a744db74154c64064d471cf08689b6366d3c6dc
hanwon <hanwon000128@gmail.com>
2025-04-17 10:16:11 +0900

    주민번호검증_우편번호검색기능_추가

--- regist.js
+++ regist.js
@@ -50,6 +50,44 @@
 //   return true;
 // }
 
+window.onload = function () {
+  const ssn1 = document.getElementById("ssn1");
+  ssn1.addEventListener("keyup", () => {
+    if (ssn1.value.length >= 6) {
+      document.getElementById("ssn2").focus();
+    }
+  });
+
+  //주민 번호 검증 통과하고 나서 하나라도 바꾸면 다시 검증 버튼 눌러야하는 상태로 바꿈
+  const ssn = document.querySelectorAll(".ssn");
+  ssn.forEach((s) => {
+    s.addEventListener("input", () => {
+      document.getElementById("ssncheck").value = "n";
+    });
+  });
+};
+
+//주민번호 검증 실습 코드
+function checkssn() {
+  const ssn1 = document.getElementById("ssn1");
+  const ssn2 = document.getElementById("ssn2");
+  const ssn = ssn1.value + ssn2.value;
+
+  const weights = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5];
+  let result = 0;
+  for (let i = 0; i < weights.length; i++) {
+    result += parseInt(ssn[i] * weights[i]);
+  }
+
+  result = (11 - (result % 11)) % 10;
+
+  if (result == parseInt(ssn[12])) {
+    alert("유효한 주민번호");
+  } else {
+    alert("주민번호 불일치");
+  }
+}
+
 function sendit() {
   const userid = document.getElementById("userid");
   const userpw = document.getElementById("userpw");
@@ -63,7 +101,8 @@ function sendit() {
     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
   const expNameText = /^[가-힣]+$/;
   const expHpText = /^\d{3}-\d{3,4}-\d{4}$/;
-  const expemailtext = /^[A-Za-z0-9@\-]+@[A-Za-z0-9\-]+\.[A-Za-z]{2,}$/;
+  //const expemailtext = /^[A-Za-z0-9@\-]+@[A-Za-z0-9\-]+\.[A-Za-z]{2,}$/;
+  const expEmailText = /^[A-Za-z0-9\-\.]+@[A-Za-z0-9\-]+\.[A-Za-z]+$/;
 
   if (userid.value === "") {
     alert("아이디를 입력해주세요.");
